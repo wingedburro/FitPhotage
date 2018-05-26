7b549b880f8780b76c9e6e4e7f535d85ebd42ba2
Kevin Liao <kliao2016@gmail.com>
2018-05-26 18:33:10 -0400

    first commit

---
+++ FitPhotage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FitPhotage
+//
+//  Created by Kevin Liao on 5/26/18.
+//  Copyright © 2018 Winged Burro Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+

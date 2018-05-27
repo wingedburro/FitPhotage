f3a9edb06494399479430efd0355e1e65d3a3d95
Kevin Liao <kliao2016@gmail.com>
2018-05-27 12:35:38 -0400

    fixed login bugs

--- FitPhotage/AppDelegate.swift
+++ FitPhotage/AppDelegate.swift
@@ -37,6 +37,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
     
     // Handle Google sign in process
     func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error?) {
+        if let error = error {
+            return
+        }
+        
         guard let authentication = user.authentication else { return }
         let credential = GoogleAuthProvider.credential(withIDToken: authentication.idToken, accessToken: authentication.accessToken)
         
